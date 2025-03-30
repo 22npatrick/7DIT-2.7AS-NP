b94e2034e9da1e184744341a3e6aee1be94e5b7c
22npatrick <22npatrick@wakatipu.school.nz>
2025-03-31 11:22:06 +1300

    Initial version of delivery function made

--- Wacdonalds_Ordering.py
+++ Wacdonalds_Ordering.py
@@ -1,24 +1,24 @@
+"""A program that can be used by Wacdonalds Queenstown to get receipt."""
 
-"""A program that can be used at Wacdonalds Queenstown to enter customer details, food orders and pick-up or delivery requirements into a computer and have it display the delivery details, itemised order, and total cost.
-"""
 import os
 import time
 
 
-# 2D list of customer_details in which there is 
-# order in name, adress, phone number
-customer_details = [["NAME1", "ADDRESS1", "PHONE NUMBER1"], 
-                    ["NAME2", "ADDRESS2", "PHONE NUMBER2"]]
+# 2D list of customer details ordered in name, adress, phone number
+customer_details = [["NAME1", "ADDRESS1", "PHONE NUMBER1"], ["NAME2", "ADDRESS2", "PHONE NUMBER2"]]
+
+order_details = ["ORDER1", "ORDER2"]
 
 
-#Sends error message to user and clears screen
 def error_message():
+    """Send error message to user and clears screen."""
     print("Invalid Input")
     time.sleep(2)
     os.system("cls")
 
-#Checks if value is a 
+
 def pos_int_input_validation(ques):
+    """Check if value is a postive interager and if not makes user input again."""
     while True:
         try:
             chosen_int = int(input(ques))
@@ -29,19 +29,66 @@ def pos_int_input_validation(ques):
         except ValueError:
             error_message()
 
-# For delivery 
-def delivery():
-    print("BLANK")
-# For pickup
-def pickup():
-    print("BLANK")
 
-# For menu 
-def menu():
+def non_zero_len_string(ques):
+    """Check if string is a string with a length over 0 and if not makes the user input again."""
     while True:
-        "Is the order delivery or pickup?"
-        "Type 1 for Delivery "
-        "Type 2 for Pickup"
+        answer_string = input(ques).strip()
+        if len(answer_string) <= 0:
+            error_message()
+        else:
+            return answer_string
+
+
+def order():
+    """Ask the Wacdonald's employee what the customer has ordered."""
+    print("")
+
+
+def delivery():
+    """Ask the Wacdonald's employee what their name, address, phone numer and prints their receipt."""
+    os.system('cls')
+    print("BLANK1")
+    name = non_zero_len_string("Type in the customers name:  ")
+    street_number = pos_int_input_validation("Type in the customer's street number: ")
+    street_name = non_zero_len_string("Type in the customer's street name: ")
+    suburb_or_locality = non_zero_len_string("Type in the customer's suburb or locality:   ")
+    city_or_town = non_zero_len_string("Type in the customer's city or town:  ")
+    while True:
+        phone_number = pos_int_input_validation("Type in the customer's phone number (in format XXXXXXXX where the phone number can only have between 7 and 11 digits): ")
+        if 7 <= len(str(phone_number)) <= 11:
+            break
+        else:
+            error_message()
+    customer_info = []
+    customer_info.append(name)
+    address_tuple = (str(street_number), street_name, suburb_or_locality, city_or_town)
+    print(address_tuple)
+    address = " ".join(address_tuple)
+    customer_info.append(address)
+    customer_info.append(phone_number)
+    print(customer_info)
+    customer_details.append(customer_info)
+    print(customer_details)
+
+
+def pickup():
+    """For pickup."""
+    print("BLANK2")
+
+
+def order_history():
+    """For order_history."""
+    print("BLANK3")
+
+
+def menu():
+    """Menu system for program that runs at the start of the program."""
+    while True:
+        print("Is the order delivery or pickup?")
+        print("Type 1 for Delivery ")
+        print("Type 2 for Pickup")
+        print("Type 3 for order history")
         choice = pos_int_input_validation(":    ")
         if choice == 1:
             delivery()
@@ -49,9 +96,11 @@ def menu():
         elif choice == 2:
             pickup()
             break
+        elif choice == 3:
+            order_history()
+            break
         else:
             error_message()
-        
+
+
 menu()
-
-
